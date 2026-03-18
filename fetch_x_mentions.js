ce3f2df3f67e36ba0e8c4bc6ebfd9c94311f7ed6
DRYICE77 <91396930+DRYICE77@users.noreply.github.com>
2026-03-18 15:37:53 -0400

    Update fetch_x_mentions.js

--- fetch_x_mentions.js
+++ fetch_x_mentions.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 
 // Node 18+ has global fetch
 
-const pool = new Pool({x
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl:
     process.env.PGSSLMODE === "disable"
